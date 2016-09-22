cb729ce1401ddc0f28139fd813f75f437994c663
UW4 <mailtosid.s@gmail.com>
2016-09-22 13:28:06 -0500

    Modify UART_Init

--- uart.c
+++ uart.c
@@ -16,25 +16,23 @@
 
 void UART_Init(uint16_t baudrate)
 {
-	//SMOD1
 	SMOD0 = 0;
-
-	PCON
+	PCON = ((SMOD1<<7)|(SMOD0<<6));
 
 	//Initialiazing SCON register
-	SM0 = 0; SM1 = 1; //Mode 1
-	REN = 1;
-	SM2 = 0;
-	TI = 0;
-	RI = 0;
+		SM0 = 0; SM1 = 1; //Mode 1
+		REN = 1;
+		SM2 = 0;
+		TI = 0;
+		RI = 0;
 
 	//Disabling Interrupt
-	ES =0;
+		ES =0;
 
 	TI =1;
 
 	BDRCON = 0;
-	//BRL = value
+	BRL = baudrate;
 	BDRCON = (0x1C|(SPD<<1));
 
 //UART Initialized
