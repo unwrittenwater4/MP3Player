b3416a75acf2ad8e52e674941add60477fb80351
Sudharsan Vaidhun <vbsudharsan@gmail.com>
2016-09-25 20:53:10 -0500

    Add LCD

--- lcd.c
+++ lcd.c
@@ -1,2 +1,31 @@
 #include "main.h"
+#include "port.H"
+#include "delay.h"
 #include "LCD.h"
+
+void LCD_Init(void) {
+	delay50ms(50);
+	LCD_Write(COMMAND, (0x30|TWOLINE|DISPLAYON));
+	delay50ms(5);
+	LCD_Write(COMMAND, (0x30|TWOLINE|DISPLAYON));
+	delay50ms(5);
+	LCD_Write(COMMAND, (0x30|TWOLINE|DISPLAYON));
+	delay50ms(5);
+	LCD_Write(COMMAND, (0x30|TWOLINE|DISPLAYON));
+	delay50ms(5);
+	LCD_Write(COMMAND, (0x08|DISPLAYON|CURSOROFF|BLINKOFF));
+	delay50ms(5);
+	LCD_Write(COMMAND, (DISPLAYCLEAR));
+	delay50ms(5);
+	LCD_Write(COMMAND, (0x04|INCREMENT|SHIFTOFF));
+	delay50ms(5);
+}
+
+void LCD_Write(bit registerSelect, uint8_t writeValue) {
+	lcdRS = registerSelect;
+	lcdEnable = 1;
+	lcdPort = writeValue;
+	delay50ms(1);
+	lcdEnable = 0;
+	lcdPort = 0xFF;
+}
--- lcd.h
+++ lcd.h
@@ -1,6 +1,40 @@
+//---------------------------------//
+// Header file to initailize lcd.c
+//---------------------------------//
+//---------------------------------//
+//Project: MP3 Player
+//Author:   Singh, Siddharth;
+//          Vaidhun, Sudharsan;
+//          Sengupta, Ayush
+//---------------------------------//
+
 #ifndef _LCD_H
-define _LCD_H
+#define _LCD_H
 
 #include "main.h"
+
+#define COMMAND 0
+#define DATA 1
+
+#define SET_ADDRESS 0x80
+#define LINE1       0x00
+#define LINE2       0x40
+#define ONELINE     0x00
+#define TWOLINE     0x08
+#define DISPLAYOFF  0x00
+#define DISPLAYON   0x04
+#define DISPLAYCLEAR 0x01
+#define CURSOROFF   0x00
+#define CURSORON    0x02
+#define BLINKOFF    0x00
+#define BLINKON     0x01
+#define DECREMENT   0x00
+#define INCREMENT   0x02
+#define SHIFTOFF    0x00
+#define SHIFTON     0x01
+
+// Functions to be used in lcd
+void LCD_Init(void);
+void LCD_Write(bit registerSelect, uint8_t writeValue);
 
 #endif
