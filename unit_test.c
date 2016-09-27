c8de6bd078871b44ded22cb2e6d4f54786a428a4
Sudharsan Vaidhun <svqm5@mst.edu>
2016-09-27 16:25:23 -0500

    Blink moved away from UnitTest

--- unit_test.c
+++ unit_test.c
@@ -1,28 +1,2 @@
 #include "unit_test.h"
 
-// Stores the LED state
-bit LED_state_R;
-bit LED_state_A;
-bit LED_state_G;
-bit LED_state_Y;
-
-void LED_FLASH_Init(void)
-{
-   LED_state_R = 0;
-}
-
-void LED_FLASH_Change_State(void)
-   {
-   // Change the LED from OFF to ON (or vice versa)
-   if (LED_state_R == 1)
-      {
-      LED_state_R = 0;
-      LED_pin_R = 0;
-      }
-   else
-      {
-      LED_state_R = 1;
-      LED_pin_R = 1;
-      }
-   }
-
--- unit_test.h
+++ unit_test.h
@@ -3,15 +3,4 @@
 
 #include "main.h"
 
-// LED is to be connected to this pin
-sbit LED_pin_R = P2^4;
-sbit LED_pin_A = P2^5;
-sbit LED_pin_G = P2^6;
-sbit LED_pin_Y = P2^7;
-
-// Function prototypes
-void LED_FLASH_Init(void);
-void LED_FLASH_Change_State(void);
-
-
 #endif
