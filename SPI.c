bb026360ef4598ab57413433a4a0cb751d745111
UW4 <mailtosid.s@gmail.com>
2016-10-18 20:35:22 -0500

    SPI source code

---
+++ SPI.c
@@ -0,0 +1,15 @@
+//---------------------------------//
+// SPI source code
+//---------------------------------//
+//---------------------------------//
+//Project: MP3 Player
+//Author: Singh, Siddharth; Vaidhun, Sudharshan;
+//		  Sengupta, Ayush
+//---------------------------------//
+
+
+#include "SPI.h"
+
+uint8_t SPI_Master_Init(uint32_t clock_rate){
+	
+}
