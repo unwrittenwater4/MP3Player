08928b82f258ec2a9bb1ec87448e01539320de74
Sudharsan Vaidhun Bhaskar <vbsudharsan@gmail.com>
2016-10-24 19:31:58 -0500

    SD Card Cleanup

--- SD_Card.c
+++ SD_Card.c
@@ -1,143 +1,137 @@
-//---------------------------------//
+//------------------------------------------------------------------//
 // SD source code
-//---------------------------------//
-//---------------------------------//
+//------------------------------------------------------------------//
+//------------------------------------------------------------------//
 //Project: MP3 Player
-//Author: Singh, Siddharth; Vaidhun, Sudharshan;
-//		  Sengupta, Ayush
-//---------------------------------//
+//Author: Singh, Siddharth; Vaidhun, Sudharsan; Sengupta, Ayush;
+//------------------------------------------------------------------//
 
 #include "Main.h"
 #include "SD_Card.h"
 #include "SPI.h"
 
-
-
-
-
-
-
 //SD Card Command
 //Inputs:
 //Output:
-uint8_t send_command(uint8_t CMD_value, uint32_t argument){
-	uint8_t ret_val, send_val, recieved_resp = 0, error_flag;
+uint8_t SD_Send_Command(uint8_t CMD_value, uint32_t argument) {
+	uint8_t return_value, send_value, recieved_response = 0, error_flag;
 	
-	if(CMD_value < 64){
-		ret_val = SD_NO_ERRORS;
+	if(CMD_value < 64) {
+		return_value = SD_NO_ERRORS;
 		
 		//Send Command
-		send_val = 0x40 | CMD_value;
-		error_flag = SPI_Transfer (send_val, &recieved_resp);
+		send_value = 0x40 | CMD_value;
+		error_flag = SPI_Transfer (send_value, &recieved_response);
 
 		//Send Argument starting MSB to LSB
-		send_val = (uint8_t)(argument >> 24);
-		error_flag = SPI_Transfer (send_val, &recieved_resp);
-		send_val = (uint8_t)(argument >> 16);
-		error_flag = SPI_Transfer (send_val, &recieved_resp);
-		send_val = (uint8_t)(argument >> 8);
-		error_flag = SPI_Transfer (send_val, &recieved_resp);
-		send_val = (uint8_t)(argument);
-		error_flag = SPI_Transfer (send_val, &recieved_resp);
+		send_value = (uint8_t)(argument >> 24);
+		error_flag = SPI_Transfer (send_value, &recieved_response);
+		send_value = (uint8_t)(argument >> 16);
+		error_flag = SPI_Transfer (send_value, &recieved_response);
+		send_value = (uint8_t)(argument >> 8);
+		error_flag = SPI_Transfer (send_value, &recieved_response);
+		send_value = (uint8_t)(argument);
+		error_flag = SPI_Transfer (send_value, &recieved_response);
 
 		if (CMD_value == CMD0){
-			send_val = 0x95;
+			send_value = 0x95;
 		}
-
 		else if (CMD_value == CMD8){
-			send_val = 0x87;
+			send_value = 0x87;
 		}
-
 		else {
-			send_val = 0x01;
+			send_value = 0x01;
 		}
 
-		error_flag = SPI_Transfer(send_val, &recieved_resp);
+		error_flag = SPI_Transfer(send_value, &recieved_response);
 
+	} else {
+		return_value = SD_ILLEGAL_COMMAND;
 	}
-	else ret_val = SD_ILLEGAL_COMMAND;
 
-	return ret_val;
+	return return_value;
 }
 
-uint8_t recieve_response(uint8_t number_of_bytes, uint8_t* array_name){
-	uint8_t time_out = 0, send_val, SPI_retval, index, ret_val, error_flag;
+uint8_t SD_Recieve_Response(uint8_t number_of_bytes, uint8_t* array_name){
+	uint8_t time_out = 0, send_value, SPI_retval, index, return_value, error_flag;
 	//Wait for R1 response
 	//Repeatedly send 0xFF until recieved value != 0xFF
 
-	do{
-		send_val = 0xFF;
-		error_flag = SPI_Transfer(send_val, &SPI_retval);
+	do {
+		send_value = 0xFF;
+		error_flag = SPI_Transfer(send_value, &SPI_retval);
 		time_out++;
-	}while((SPI_retval == 0xFF) && (time_out != 0));
+	} while((SPI_retval == 0xFF) && (time_out != 0));
 
-	if (time_out == 0 ) ret_val = SD_TIMEOUT_ERROR;
-	else *array_name = SPI_retval;
+	if (time_out == 0 )
+		return_value = SD_TIMEOUT_ERROR;
+	else
+		*array_name = SPI_retval;
 
 	if((SPI_retval == 0x01) || (SPI_retval == 0x00))		//0x01 -> Idle state; 0x00-> active state
 	{
 		if (number_of_bytes > 1){
-			for(index = 1; index < number_of_bytes; index++ ){
-				error_flag = SPI_Transfer ( 0xFF, &SPI_retval);
+			for(index = 1; index < number_of_bytes; index++) {
+				error_flag = SPI_Transfer (0xFF, &SPI_retval);
 				*(array_name + index) = SPI_retval; 
 			}
 		}
-
-		ret_val = error_flag;
+		return_value = SD_NO_ERRORS;
 	}
-
+	else
+		return_value = SD_RESPONSE_ERROR;
 
 	//Clearing Buffer
 	error_flag = SPI_Transfer(0XFF, &SPI_retval);
 
-	return ret_val;
+	return return_value;
 }
 
 
 //SD Card Initialization
 //Inputs: None
 //Outputs: None
-void SD_Init(void){
-	uint8_t recieved_resp[5];
-	uint8_t error_flag, error_status, iter = 0;
+uint8_t SD_Init(void){
+	uint8_t recieved_response[5];
+	uint8_t error_flag, return_value, error_status, iter = 0;
 
 	//Power On
 	SD_Select = 1;
-	for( iter = 0; iter < 10; iter++){
-		error_flag = SPI_Transfer(0xFF , &recieved_resp);
+	for(iter = 0; iter < 10; iter++){
+		error_flag = SPI_Transfer(0xFF , &recieved_response);
 	}
 
 	//CMD0
 	SD_Select = 0;
-	error_flag = send_command(CMD0,0);
-	error_flag = recieve_response(1, &recieved_resp);
+	error_flag = SD_Send_Command(CMD0,0);
+	error_flag = SD_Recieve_Response(1, &recieved_response);
 	SD_Select  =1;
 
-	if(error_flag == SD_NO_ERRORS){
-
-		if (recieved_resp[0] = 0X01)	error_status = SD_NO_ERRORS;
-
-		else 	error_status = SD_RESPONSE_ERROR;
-
+	if(error_flag == SD_NO_ERRORS) {
+		if (recieved_response[0] = 0x01)
+			error_status = SD_NO_ERRORS;
+		else
+			error_status = SD_RESPONSE_ERROR;
 	}
 
-
 	//CMD8
-	if (error_status == SD_NO_ERRORS){
+	if (error_status == SD_NO_ERRORS) {
 		SD_Select = 0;
-		error_flag = send_command(CMD8,0X000001AA);
-		error_flag = recieve_response(5, &recieved_resp);	//R1+R7 response
+		error_flag = SD_Send_Command(CMD8,0x000001AA);
+		error_flag = SD_Recieve_Response(5, &recieved_response);	//R1+R7 response
 		SD_Select = 1;
 
-		if (error_flag == SD_NO_ERRORS){
-			if(recieved_resp[0] == 0X05){
-				//OLd CARD
+		if (error_flag == SD_NO_ERRORS) {
+			if (recieved_response[0] == 0x05) {
+				//Old SD Card
 				error_status = SD_VERSION_1;
 			}
 
-			if (recieved_resp[0] == 0X01) 	error_status = SD_NO_ERRORS;
-			//else	printf( " Voltage Problem \n ");
-
+			if (recieved_response[0] == 0X01) {
+				error_status = SD_NO_ERRORS;
+			}
+			else
+				error_status = SD_VOLTAGE_ERROR;
 		}
 	}
 
@@ -146,13 +140,13 @@ void SD_Init(void){
 	/*
 	if (error_status == SD_NO_ERRORS){
 		SD_Select = 0;
-		error_flag = send_command(CMD58, 0X000001AA);
-		error_flag = recieve_response(5, &recieved_resp);		//R1+R3 response
+		error_flag = SD_Send_Command(CMD58, 0X000001AA);
+		error_flag = SD_Recieve_Response(5, &recieved_response);		//R1+R3 response
 		SD_Select = 1;
 
 		if (error_flag == SD_NO_ERRORS){
-			if (recieved_resp[0] == 0X01){
-				if (recieved_resp[3] == ?)
+			if (recieved_response[0] == 0X01){
+				if (recieved_response[3] == ?)
 			}
 		}
 	}
@@ -164,19 +158,18 @@ void SD_Init(void){
 
 	if (error_status == SD_NO_ERRORS){
 		SD_Select = 0;
-		error_flag = send_command(CMD55, 0X000001AA);
-		error_flag = recieve_response(1, &recieved_resp);
+		error_flag = SD_Send_Command(CMD55, 0X000001AA);
+		error_flag = SD_Recieve_Response(1, &recieved_response);
 		SD_Select = 1;
 
-		if (recieved_resp[0] == 0x01){
+		if (recieved_response[0] == 0x01){
 			SD_Select = 0;
-			error_flag = send_command(CMD41, 0X000001AA);
-			error_flag = recieve_response(1, &recieved_resp);
+			error_flag = SD_Send_Command(CMD41, 0X000001AA);
+			error_flag = SD_Recieve_Response(1, &recieved_response);
 			SD_Select = 1;
 
 
 		}
 	}
-
-
+	return return_value;
 }
--- SD_Card.h
+++ SD_Card.h
@@ -1,44 +1,35 @@
-//---------------------------------//
+//------------------------------------------------------------------//
 // Header file for initailize SD_Card
-//---------------------------------//
-//---------------------------------//
+//------------------------------------------------------------------//
+//------------------------------------------------------------------//
 //Project: MP3 Player
-//Author: Singh, Siddharth; Vaidhun, Sudharshan;
-//		  Sengupta, Ayush
-//---------------------------------//
+//Author: Singh, Siddharth; Vaidhun, Sudharsan; Sengupta, Ayush;
+//------------------------------------------------------------------//
 
 
 #ifndef _SD_CARD_H
 #define _SD_CARD_H
 
-
 #include "main.h"
 #include "PORT.H"
 
-
 #define CMD0					0
 #define CMD8					8
-
 #define CMD41					41
 #define CMD42					42
 #define CMD55					55
 #define CMD58					58
-
-
 
 #define SD_NO_ERRORS		 	0
 #define SD_ILLEGAL_COMMAND 		-11
 #define SD_TIMEOUT_ERROR		-12		
 #define SD_RESPONSE_ERROR		-13
 #define SD_VERSION_1			-14
-
+#define SD_VOLTAGE_ERROR		-15
 
 //Function Prototypes
-void SD_Init(void);
-uint8_t send_command(uint8_t , uint32_t );
-uint8_t recieve_response(uint8_t , uint8_t* );
-
-
-
+uint8_t SD_Init(void);
+uint8_t SD_Send_Command(uint8_t, uint32_t);
+uint8_t SD_Recieve_Response(uint8_t, uint8_t*);
 
 #endif
